db080103c9ec274e7926e9f8cdda42a78d058d42
mariohamann <mario.hamann@virtual-identity.com>
2026-01-09 12:45:07 +0100

    add types

---
+++ src/integrations/script-island/ScriptIsland.si.d.ts
@@ -0,0 +1,8 @@
+import type { ComponentProps } from 'astro/types';
+
+interface ScriptIslandProps {
+  children?: any;
+}
+
+declare const ScriptIsland: (props: ScriptIslandProps) => null;
+export default ScriptIsland;
